66de96e59786957e41610915053ce6465928e472
JustForFun_ <66706701+JFF-22@users.noreply.github.com>
2025-12-13 08:56:48 +0800

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,22 @@ int main() {
     cout << "##### Food Ordering System with Demand and Profit Tracking #####\n" << endl;
 
     Menu menu;
-    printVectorValues(menu.menuList(false, true));
+    Order order;
 
+    // ID, Name, Price, Category
+    vector<map<string, string>> menuList = menu.menuList(false, true);
+
+    cout<<"---------- Menu ----------"<<endl;
+    for (auto& item:menuList){
+        cout<<item.at("id") + " | " + item.at("name") + " | RM " + item.at("selling_price") + " | " + item.at("category") << endl;
+    }
+    cout<<"\n"<<endl;
+
+    string input = "";
+    while (input != "X" && input != "x"){
+        cout<<"Choose menu or close (X): ";
+        cin>>input;
+    }
 
     system("pause"); // waits for a keypress
 
